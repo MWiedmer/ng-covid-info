dfa462b600146c6a790498a79e6078a0cc1e7fad
Markus Wiedmer <markus.wiedmer@bs.ch>
2021-08-24 18:03:02 +0200

    initial release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BagFetcherService} from "./bag-fetcher.service";
+import {Cases} from "./model/Cases";
+import {Observable} from "rxjs";
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'covid-info';
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
+  cases: Cases[] = [];
+
+
+  constructor(private bagFetcherService: BagFetcherService) {
+    this.getCases();
+  }
+
+  getCases(): void {
+    this.bagFetcherService.getCases()
+        .subscribe(cases => this.cases = cases);
+  }
+
+  getFilteredCases(): Cases[] {
+    return this.cases.filter(cases => cases.geoRegion = 'CH');
+  }
+
 }
--- src/app/bag-fetcher.service.ts
+++ src/app/bag-fetcher.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from "rxjs";
+import {Cases} from "./model/Cases";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BagFetcherService {
 
-  constructor() { }
+  private casesUrl = 'https://www.covid19.admin.ch/api/data/20210824-jrvdp23s/sources/COVID19Cases_geoRegion.json';
+  private contextUrl = 'https://www.covid19.admin.ch/api/data/context';  // URL to web api
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  /** GET heroes from the server */
+   getCases(): Observable<Cases[]> {
+    return this.http.get<Cases[]>(this.casesUrl)
+  }
+
 }
